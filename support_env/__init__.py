ae8ede43d44829ba899c550edc5c17a1e06131c5
lockrock786-max <lock.rock786@gmail.com>
2026-04-10 18:07:59 +0530

    Update __init__.py

--- support_env/__init__.py
+++ support_env/__init__.py
@@ -1,4 +1,4 @@
-from .environment import SupportTicketEnv
+from .environment import SupportTicketEnv, TASKS
 from .graders import grade_task
 from .models import Action, EnvState, Observation, Reward, TaskSpec, TicketScenario
 from .scenarios import TASKS
